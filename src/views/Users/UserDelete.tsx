c58dd2eecee6deb123ca91ed66c8a8796ac9f4d3
rczerwiec <czerwiecr1999@gmail.com>
2023-02-12 15:33:10 +0100

    modal tests and fixes

--- src/views/Users/UserDelete.tsx
+++ src/views/Users/UserDelete.tsx
@@ -10,26 +10,32 @@ function UserDelete({ onDelete, user }: IProps) {
   const { isVisible, closeModal, toggleModal } = useModal();
 
   return (
-    <div
-      onClick={() => {
-        toggleModal();
-      }}
-    >
-      Delete
+    <div>
+      <div
+        onClick={() => {
+          toggleModal();
+        }}
+      >
+        Delete
+      </div>
       <Modal isVisible={isVisible} onClose={closeModal}>
-        <p>Are you sure you want to delete this user?</p>
-        <button
-          type="button"
-          onClick={() => {
-            onDelete(user);
-            closeModal();
-          }}
-        >
-          Yes
-        </button>
-        <button type="button" onClick={closeModal}>
-          Cancel
-        </button>
+        <div className="absolute bg-whiteMain mt-20 h-full w-1/2 left-1/4 top-0 bg-white rounded ">
+          <div className="absolute flex flex-col shrink h-full w-full overflow-y-auto scrollbar-hide">
+            <p>Are you sure you want to delete this user?</p>
+            <button
+              type="button"
+              onClick={() => {
+                onDelete(user);
+                closeModal();
+              }}
+            >
+              Yes
+            </button>
+            <button type="button" onClick={closeModal}>
+              Cancel
+            </button>
+          </div>
+        </div>
       </Modal>
     </div>
   );
--- src/views/Users/UserEdit.tsx
+++ src/views/Users/UserEdit.tsx
@@ -1,5 +1,5 @@
 import { Modal, useModal } from "../../shared/components/Modal";
-import {useFormik} from 'formik';
+import { useFormik } from "formik";
 import { IUser } from "../../shared/globalTypes";
 import TextInput from "../../shared/components/TextInput";
 
@@ -25,35 +25,108 @@ function UserEdit({ onEdit, user }: IProps) {
       bs: user.company.bs,
       companyName: user.company.name,
     },
-    onSubmit: values => {
+    onSubmit: (values) => {
+      onEdit(user);
       alert(JSON.stringify(values, null, 2));
+      closeModal();
     },
   });
 
   //Formularz do osobnego kompentu później
   return (
-    <div
-      onClick={() => {
-        toggleModal();
-        onEdit(user);
-      }}
-    >
-      Edit
+    <div>
+      <div
+        onClick={() => {
+          toggleModal();
+        }}
+      >
+        Edit
+      </div>
       <Modal isVisible={isVisible} onClose={closeModal}>
-      <form onSubmit={formik.handleSubmit}>
-        <TextInput name="username" type="text" value={formik.values.username} label="Username" onChange={formik.handleChange} />
-        <TextInput name="name" type="text" value={formik.values.name} label="Name" onChange={formik.handleChange} />
-        <TextInput name="email" type="text" value={formik.values.email} label="Email" onChange={formik.handleChange} />
-        <TextInput name="phone" type="text" value={formik.values.phone} label="Phone" onChange={formik.handleChange} />
-        <TextInput name="website" type="text" value={formik.values.website} label="Website" onChange={formik.handleChange} />
-        <TextInput name="city" type="text" value={formik.values.city} label="City" onChange={formik.handleChange} />
-        <TextInput name="street" type="text" value={formik.values.street} label="Street" onChange={formik.handleChange} />
-        <TextInput name="zipcode" type="text" value={formik.values.zipcode} label="Zipcode" onChange={formik.handleChange} />
-        <TextInput name="suite" type="text" value={formik.values.suite} label="Suite" onChange={formik.handleChange} />
-        <TextInput name="bs" type="text" value={formik.values.bs} label="Bs" onChange={formik.handleChange} />
-        <TextInput name="companyName" type="text" value={formik.values.companyName} label="CompanyName" onChange={formik.handleChange} />
-       <button type="submit">Submit</button>
-     </form>
+        <div className="absolute bg-whiteMain mt-20 h-full w-1/2 left-1/4 top-0 bg-white rounded ">
+          <div className="absolute flex flex-col shrink h-full w-full overflow-y-auto scrollbar-hide">
+            <form onSubmit={formik.handleSubmit}>
+              <TextInput
+                name="username"
+                type="text"
+                value={formik.values.username}
+                label="Username"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="name"
+                type="text"
+                value={formik.values.name}
+                label="Name"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="email"
+                type="text"
+                value={formik.values.email}
+                label="Email"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="phone"
+                type="text"
+                value={formik.values.phone}
+                label="Phone"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="website"
+                type="text"
+                value={formik.values.website}
+                label="Website"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="city"
+                type="text"
+                value={formik.values.city}
+                label="City"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="street"
+                type="text"
+                value={formik.values.street}
+                label="Street"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="zipcode"
+                type="text"
+                value={formik.values.zipcode}
+                label="Zipcode"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="suite"
+                type="text"
+                value={formik.values.suite}
+                label="Suite"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="bs"
+                type="text"
+                value={formik.values.bs}
+                label="Bs"
+                onChange={formik.handleChange}
+              />
+              <TextInput
+                name="companyName"
+                type="text"
+                value={formik.values.companyName}
+                label="CompanyName"
+                onChange={formik.handleChange}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
       </Modal>
     </div>
   );
